d02f39e733ef02142d492a67691dd75a6880a159
Boiko Konstantin <92636892+tipachel88@users.noreply.github.com>
2021-10-22 17:46:53 +0300

    first

--- Homework 2/ViewController.swift
+++ Homework 2/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorWindow: UIView!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorWindow.layer.cornerRadius = 10
     }
-
-
+    func changeColor(){
+        colorWindow.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    func changeValueForLabel(){
+        redLabel.text = "Red: \(round(redSlider.value * 10) / 10)"
+        greenLabel.text = "Green: \(round(greenSlider.value * 10) / 10)"
+        blueLabel.text = "Blue: \(round(blueSlider.value * 10) / 10)"
+    }
+    
+    @IBAction func redSliderAction(_ sender: Any) {
+        changeColor()
+        changeValueForLabel()
+    }
+    @IBAction func greenSliderAction(_ sender: Any) {
+        changeColor()
+        changeValueForLabel()
+    }
+    @IBAction func blueSliderAction(_ sender: Any) {
+        changeColor()
+        changeValueForLabel()
+    }
+    
 }
+
 
